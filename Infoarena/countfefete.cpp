d15639412a135314cbf3d21308ca4d4d816a2243
stefdasca <cosmin_stefan_d@yahoo.com>
2018-12-27 21:08:48 +0200

    Add files via upload

---
+++ Infoarena/countfefete.cpp
@@ -0,0 +1,156 @@
+#include<bits/stdc++.h>
+#define mod 1000000007
+using namespace std;
+ifstream f("countfefete.in");
+ofstream g("countfefete.out");
+int n, val[200002];
+int szc[200002];
+long long ans = 0;
+vector<int>v[200002];
+long long doi[200002];
+long long fact[200002], inv[200002];
+long long pw(long long b, long long e)
+{
+    long long ans = 1;
+    while(e)
+    {
+        if(e & 1)
+            ans = (ans * b) % mod;
+        b = (b * b) % mod;
+        e >>= 1;
+    }
+    return ans;
+}
+int nr[200002];
+bool cmp(int a, int b)
+{
+    return val[a] < val[b];
+}
+long long zeroes[200002][30], ones[200002][30];
+int tt[200002], rg[200002];
+int Find(int a)
+{
+    if(tt[a] == a)
+        return a;
+    return tt[a] = Find(tt[a]);
+}
+void Union(int a, int b)
+{
+    if(rg[a] > rg[b])
+    {
+        tt[b] = a, rg[a] += rg[b];
+        for(int i = 29; i >= 0; --i)
+            zeroes[a][i] += zeroes[b][i], ones[a][i] += ones[b][i];
+    }
+    else
+    {
+        tt[a] = b, rg[b] += rg[a];
+        for(int i = 29; i >= 0; --i)
+            zeroes[b][i] += zeroes[a][i], ones[b][i] += ones[a][i];
+    }
+}
+bool viz[200002];
+long long sumzeroes[32], sumones[32], dropodd[32], dropeven[32];
+int main()
+{
+    f >> n;
+    fact[0] = 1;
+    inv[0] = 1;
+    for(int i = 1; i <= n; ++i)
+        fact[i] = (fact[i-1] * i) % mod, inv[i] = pw(fact[i], mod - 2);
+    doi[0] = 1;
+    for(int i = 1; i <= n; ++i)
+        doi[i] = (doi[i-1] * 2) % mod;
+    for(int i = 1; i <= n; ++i)
+        f >> val[i];
+    for(int i = 1; i < n; ++i)
+    {
+        int a, b;
+        f >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for(int i = 1; i <= n; ++i)
+        nr[i] = i;
+    sort(nr + 1, nr + n + 1, cmp);
+    for(int j = 1; j <= n; ++j)
+    {
+        tt[j] = j, rg[j] = 1;
+        viz[j] = 0;
+        for(int i = 29; i >= 0; --i)
+            if((val[j] & (1<<i)))
+                ones[j][i]++;
+            else
+                zeroes[j][i]++;
+    }
+    for(int x = n; x >= 1; --x)
+    {
+        for(int i = 29; i >= 0; --i)
+        {
+             sumzeroes[i] = zeroes[nr[x]][i];
+             sumones[i] = ones[nr[x]][i];
+             dropodd[i] = 0;
+             dropeven[i] = 0;
+        }
+        viz[nr[x]] = 1;
+        for(int j = 0; j < v[nr[x]].size(); ++j)
+        {
+            int vecin = v[nr[x]][j];
+            if(!viz[vecin])
+                continue;
+            for(int i = 29; i >= 0; --i)
+            {
+                sumzeroes[i] += zeroes[Find(vecin)][i];
+                sumones[i] += ones[Find(vecin)][i];
+                if(!ones[Find(vecin)][i])
+                {
+                    dropeven[i] += doi[zeroes[Find(vecin)][i]] - 1;
+                    while(dropeven[i] >= mod)
+                        dropeven[i] -= mod;
+                }
+                else
+                {
+                    dropodd[i] += doi[ones[Find(vecin)][i] - 1] * doi[zeroes[Find(vecin)][i]];
+                    dropodd[i] %= mod;
+                    dropeven[i] += doi[ones[Find(vecin)][i] - 1] * doi[zeroes[Find(vecin)][i]] - 1;
+                    dropeven[i] %= mod;
+                }
+            }
+            if(Find(vecin) != Find(nr[x]))
+                Union(Find(vecin), Find(nr[x]));
+        }
+        for(int i = 29; i >= 0; --i)
+        {
+            int z = ((val[nr[x]] & (1<<i)) > 0);
+            long long evencount = 0;
+            long long oddcount = 0;
+            if(!sumones[i])
+            {
+                evencount += doi[sumzeroes[i]] - 1;
+                while(evencount >= mod)
+                    evencount -= mod;
+            }
+            else
+            {
+                oddcount += doi[sumones[i] - 1] * doi[sumzeroes[i]];
+                oddcount %= mod;
+                evencount += doi[sumones[i] - 1] * doi[sumzeroes[i]] - 1;
+                evencount %= mod;
+            }
+            evencount = (evencount - dropeven[i] + mod);
+            oddcount = (oddcount - dropodd[i] + mod);
+            if(z)
+            {
+                ans += 1LL * (1<<i) * evencount;
+                ans %= mod;
+            }
+            else
+            {
+                ans += 1LL * (1<<i) * oddcount;
+                ans %= mod;
+            }
+        }
+    }
+    g << ans;
+    return 0;
+}
---
+++ Infoarena/numerex.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+using namespace std;
+ifstream f("numerex.in");
+ofstream g("numerex.out");
+long long n, m, bucketSize, cnt;
+long long v[100002];
+long long bucketsum[502];
+long long bkt[100002];
+long long st[502], sf[502];
+long long xtr[502];
+struct ranges
+{
+    long long nr, st, sf;
+};
+ranges z[502][502];
+long long countt[502];
+void add(long long stt, long long drr, long long stval)
+{
+    drr = drr + stt - 1;
+    drr = min(n, drr);
+    for(long long i = stt; i <= drr; )
+    {
+        if(i + bucketSize - 1 <= drr)
+        {
+            if(bkt[i] == bkt[i + bucketSize - 1])
+            {
+                ++countt[bkt[i]];
+                long long first = i - stt + 1;
+                long long last = i + bucketSize - stt;
+                long long gauss = 1LL * (last + 1) * last / 2 - 1LL * (first - 1) * first / 2;
+                xtr[bkt[i]] += gauss * stval;
+                z[bkt[i]][countt[bkt[i]]] = {stval, first, last};
+                i += bucketSize;
+            }
+            else
+            {
+                long long j = i;
+                for(j = i; (bkt[i] == bkt[j]); ++j)
+                    v[j] += 1LL * (j - stt + 1) * stval, bucketsum[bkt[i]] += 1LL * (j - stt + 1) * stval;
+                i = j;
+            }
+        }
+        else
+        {
+            for(long long j = i; j <= drr; ++j)
+                v[j] += 1LL * (j - stt + 1) * stval, bucketsum[bkt[j]] += 1LL * (j - stt + 1) * stval;
+            i = drr + 1;
+        }
+    }
+}
+long long compute(long long stt, long long drr)
+{
+    long long ans = 0;
+    for(long long i = stt; i <= drr; )
+    {
+      //  g << stt << " " << drr << " " << i << " " << ans << '\n';
+        if(i + bucketSize - 1 <= drr)
+        {
+            if(bkt[i] == bkt[i + bucketSize - 1])
+            {
+                ans += xtr[bkt[i]];
+                ans += bucketsum[bkt[i]];
+                i += bucketSize;
+            }
+            else
+            {
+                long long j = i;
+                for(j = i; (bkt[i] == bkt[j]); ++j)
+                    ans += v[j];
+                for(long long q = 1; q <= countt[bkt[i]]; ++q)
+                {
+                    long long endd = z[bkt[i]][q].sf;
+                    long long beginn = (z[bkt[i]][q].st + (i - 1 - st[bkt[i]]));
+                    long long gs = 1LL * endd * (endd + 1) / 2 - 1LL * beginn * (beginn + 1) / 2;
+                    ans += gs * z[bkt[i]][q].nr;
+                }
+                i = j;
+            }
+        }
+        else
+        {
+            long long j;
+            for(j = i; j <= drr; ++j)
+                ans += v[j];
+            long long sffbkti = min(drr, sf[bkt[i]]);
+            for(long long q = 1; q <= countt[bkt[i]]; ++q)
+            {
+                long long endd = z[bkt[i]][q].st + (sffbkti - st[bkt[i]]);
+                long long beginn = (z[bkt[i]][q].st + (i - st[bkt[i]])) - 1;
+                long long gs = 1LL * endd * (endd + 1) / 2 - 1LL * beginn * (beginn + 1) / 2;
+                ans += gs * z[bkt[i]][q].nr;
+            }
+            if(sffbkti < drr)
+            {
+                for(long long q = 1; q <= countt[bkt[j]]; ++q)
+                {
+                    long long endd = z[bkt[j]][q].st + (drr - st[bkt[j]]);
+                    long long beginn = z[bkt[j]][q].st - 1;
+                    long long gs = 1LL * endd * (endd + 1) / 2 - 1LL * beginn * (beginn + 1) / 2;
+                    ans += gs * z[bkt[j]][q].nr;
+                }
+            }
+            i = drr + 1;
+        }
+    }
+    return ans;
+}
+void calc()
+{
+    for(long long i = 1; i <= n; i += bucketSize)
+    {
+        long long ratie = 0;
+        long long suma = 0;
+        for(long long j = 1; j <= countt[bkt[i]]; ++j)
+            ratie +=  z[bkt[i]][j].nr, suma += 1LL * z[bkt[i]][j].nr * z[bkt[i]][j].st;
+        xtr[bkt[i]] = 0;
+        countt[bkt[i]] = 0;
+        for(long long j = i; j <= sf[bkt[i]]; ++j)
+            bucketsum[bkt[i]] += suma, v[j] += suma, suma += ratie;
+    }
+}
+int main()
+{
+    f >> n >> m;
+    bucketSize = sqrt(n);
+    cnt = 0;
+    for(long long i = 1; i <= n; i += bucketSize)
+    {
+        ++cnt;
+        st[cnt] = i;
+        sf[cnt] = min(i + bucketSize - 1, n);
+        for(long long j = i; j <= sf[cnt]; ++j)
+            bkt[j] = cnt, bucketsum[cnt] += v[i];
+    }
+    long long bucket2 = sqrt(m);
+    for(long long i = 1; i <= m; ++i)
+    {
+        long long tip, x, l, k;
+        f >> tip;
+        if(tip == 0)
+        {
+            f >> x >> l >> k;
+            add(x, l, k);
+        }
+        else
+        {
+            f >> x >> l;
+            g << compute(x, l) << '\n';
+        }
+        if(i % bucket2 == 0)
+            calc();
+    }
+    return 0;
+}
