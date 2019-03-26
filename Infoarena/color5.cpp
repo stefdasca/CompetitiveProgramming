c6d63bcfb8bcc8d0b0573649f49096c50259ded4
stefdasca <cosmin_stefan_d@yahoo.com>
2019-03-26 16:37:20 +0200

    Update color5.cpp

--- Infoarena/color5.cpp
+++ Infoarena/color5.cpp
@@ -1,3 +1,14 @@
+/*
+        Infoarena color5(https://www.infoarena.ro/problema/color5)
+       
+   "Given an undirected graph with n+1 nodes which has 2n edges(there is edge between n and every other node and there is edge between
+a and (a+1) mod n), color the edges with as few colors as possible such that for each pair of nodes, there is a path which crosses only 
+through edges colored differently"
+
+    After drawing some graphs on paper, one can observe that for each n, we can draw a 3-colorable graph. Also, for n = 3 we can use just 1
+color because it's a complete graph, while for 4 <= n <= 6, we can make it 2-colorable using a rather simple pattern.
+    
+*/
 #include<bits/stdc++.h>
 using namespace std;
 ifstream f("color5.in");
