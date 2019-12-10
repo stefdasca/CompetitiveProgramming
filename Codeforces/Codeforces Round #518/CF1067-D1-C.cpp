8ac51fc45a55178faadd1b9a92a99c32a0c18cac
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2019-12-10 22:58:19 +0200

    Add files via upload

---
+++ Codeforces/Codeforces Round #518/CF1067-D1-C.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define god dimasi5eks
+#pragma GCC optimize("O3")
+#define fi first
+#define se second
+#define pb push_back
+#define pf push_front
+#define mod 1000000007
+#define dancila 3.14159265359
+#define eps 1e-9
+
+using namespace std;
+
+typedef long long ll;
+
+
+int add(int a, int b)
+{
+    ll x = a+b;
+    if(x >= mod)
+        x -= mod;
+    if(x < 0)
+        x += mod;
+    return x;
+}
+ll mul(ll a, ll b)
+{
+    return (a*b) % mod;
+}
+
+ll pw(ll a, ll b)
+{
+    ll ans = 1;
+    while(b)
+    {
+        if(b & 1)
+            ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return ans;
+}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+long long rand_seed()
+{
+    long long a = rng();
+    return a;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int cx = 0;
+    while(n)
+    {
+        if(n)
+            cout << cx << " " << 0 << '\n', --n;
+        if(n)
+            cout << cx+1 << " " << 0 << '\n', --n;
+        if(n)
+            cout << cx+1 << " " << 3 << '\n', --n;
+        cx += 2;
+    }
+    return 0;
+}
