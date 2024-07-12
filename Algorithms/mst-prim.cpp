209a1ca81a7b74593709df07528c33e27eb375f9
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2024-07-13 01:03:33 +0200

    chore: new impl for Prim's algorithm

--- Algorithms/mst-prim.cpp
+++ Algorithms/mst-prim.cpp
@@ -1,62 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-ifstream f("apm.in");
-ofstream g("apm.out");
-int n, m;
-vector<pair<int, int> >v[200002];
-bool viz[200002], pus[200002]; 
-set<pair<int, int> >s;
-int mn[200002];
-int vc[200002];
-int q, sum;
-pair<int, int> ans[200002];
-int main()
-{
-    f >> n >> m;
-    for(int i = 1; i <= m; ++i)
-    {
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    vector<vector<pair<int, int> > > graph(n+1); 
+    for (int i = 1; i <= m; i++) {
         int a, b, c;
-        f >> a >> b >> c;
-        v[a].push_back({b, c});
-        v[b].push_back({a, c});
+        cin >> a >> b >> c;
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
     }
+    
+    vector<int> cost(n+1, (1<<30));
+    cost[1] = 0;
+    set<pair<int, int> > s;
     s.insert({0, 1});
-    viz[1] = 1;
-    for(int i = 1; i <= n; ++i)
-    {
-        pair<int, int> p = *s.begin();
-        s.erase(p);
-        swap(p.first, p.second);
-        pus[p.first] = 1;
-        if(i >= 2)
-        {
-            ans[++q] = make_pair(vc[p.first], p.first);
-            sum += mn[p.first];
-        }
-        for(int j = 0; j < v[p.first].size(); ++j)
-        {
-            int vecin = v[p.first][j].first;
-            int cost = v[p.first][j].second;
-            if(pus[vecin])
-                continue;
-            if(!viz[vecin] || cost < mn[vecin])
-            {
-                if(!viz[vecin])
-                    s.insert({cost, vecin});
-                else
-                {
-                    s.erase({mn[vecin], vecin});
-                    s.insert({cost, vecin});
+    
+    long long MST = 0;
+    int added = 0;
+    while(!s.empty()) {
+        pair<int, int> smallest = *s.begin();
+        s.erase(smallest);
+        
+        MST += smallest.first;
+        cost[smallest.second] = 0;
+        added++;
+        for (int i = 0; i < (int) graph[smallest.second].size(); i++) {
+            int nxt = graph[smallest.second][i].first;
+            int val = graph[smallest.second][i].second;
+            
+            if (val < cost[nxt]) {
+                if (s.find({cost[nxt], nxt}) != s.end()) {
+                    s.erase({cost[nxt], nxt});
                 }
-                viz[vecin] = 1;
-                mn[vecin] = cost;
-                vc[vecin] = p.first;
+                cost[nxt] = val;
+                s.insert({cost[nxt], nxt});
             }
         }
     }
-    g << sum << '\n';
-    g << n-1 << '\n';
-    for(int i = 1; i <= q; ++i)
-        g << ans[i].first << " " << ans[i].second << '\n';
+    
+    if (added == n) {
+        cout << MST << '\n';
+    }
+    else {
+        cout << "IMPOSSIBLE" << '\n';
+    }  
     return 0;
 }
