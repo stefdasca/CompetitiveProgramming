9135b4b9dccb4f7657abb4c20f9f99c6c7f4eb35
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2024-07-14 13:53:59 +0200

    Update bellman-ford.cpp

--- Algorithms/bellman-ford.cpp
+++ Algorithms/bellman-ford.cpp
@@ -1,62 +1,52 @@
 // https://www.infoarena.ro/problema/bellmanford
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-ifstream f("bellmanford.in");
-ofstream g("bellmanford.out");
-int n, m, viz[50005], d[50005];
-bool is[50005];
-struct nod
-{
-    int x, c;
-};
-vector <nod> v[50005];
-queue <int> q;
-int bellmanford(int x)
-{
-    q.push(x);
-    viz[x] = 1;
-    d[x] = 0;
-    is[x] = 1;
-    while(!q.empty())
-    {
-        x = q.front();
+
+int main() {
+    
+    ifstream cin("bellmanford.in");
+    ofstream cout("bellmanford.out");
+    
+    int n, m;
+    cin >> n >> m;
+    
+    vector<vector<pair<int, int> > > graph(n+1);
+    for (int i = 1; i <= m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        
+        graph[a].push_back({b, c});
+    }
+    
+    vector<long long> costs(n+1, (1LL<<60));
+    vector<int> cnt(n+1);
+    
+    costs[1] = 0;
+    queue<int> q; 
+    q.push(1);
+    while(!q.empty()) {
+        int node = q.front();
         q.pop();
-        is[x] = 0;
-        for(int i = 0; i < v[x].size(); i++)
-        {
-            int y = v[x][i].x;
-            if(d[y] > d[x] + v[x][i].c)
-            {
-                d[y] = d[x] + v[x][i].c;
-                viz[y]++;
-                if(viz[y] >= n-1)
+        
+        for (int i = 0; i < (int) graph[node].size(); i++) {
+            int nxt = graph[node][i].first;
+            int c = graph[node][i].second;
+            if (costs[node] + c < costs[nxt]) {
+                costs[nxt] = costs[node] + c;
+                q.push(nxt);
+                
+                cnt[nxt]++;
+                if (cnt[nxt] > n) {
+                    cout << "Ciclu negativ!" << '\n';
                     return 0;
-                if(!is[y])
-                {
-                    q.push(y);
-                    is[y]=1;
-                } 
+                }
             }
         }
     }
-    return 1;
-}
-int main()
-{
-    f >> n >> m;
-    for(int i = 1; i <= m; i++)
-    {
-        int x, y, c;
-        f >> x >> y >> c;
-        v[x].push_back({y, c});
+    
+    for (int i = 2; i <= n; i++) {
+        cout << costs[i] << " ";
     }
-    for(int i = 1; i <= n; i++)
-        d[i] = (1 << 30);
-    if(bellmanford(1) == 0)
-        g << "Ciclu negativ!";
-    else
-        for(int i = 2; i <= n; i++)
-            g << d[i] << " ";
     return 0;
 }
