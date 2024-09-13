945b884710c3ea44649e03ba448dc091a496978f
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2024-09-13 14:04:55 +0200

    Update biconnected.cpp

--- Algorithms/biconnected.cpp
+++ Algorithms/biconnected.cpp
@@ -1,60 +1,109 @@
-// https://www.infoarena.ro/problema/biconex
+// based on https://cses.fi/problemset/task/2177
 
-#include<bits/stdc++.h>
+// it finds BCCs, bridges, articulation points
+
+#include <bits/stdc++.h>
 using namespace std;
-ifstream f("biconex.in");
-ofstream g("biconex.out");
-int n, m, nr;
-vector<int>v[100002], sol[100002];
-bool viz[100002];
-int niv[100002], low[100002];
-deque<int>d;
-void dfs(int dad, int nod)
-{
-    viz[nod] = 1;
-    low[nod] = niv[nod];
-    d.push_back(nod);
-    for(int i = 0; i < v[nod].size(); ++i)
-    {
-        int vecin = v[nod][i];
-        if(vecin == dad)
-            continue;
-        if(viz[vecin])
-        {
-            low[nod] = min(low[nod], niv[vecin]);
+
+int n, m, cnt;
+vector<vector<int> > v;
+
+vector<int> vis, lvl, low;
+deque<int> d;
+
+set<pair<int, int> > edges; // for fixing directions
+
+vector<vector<int> > bccs;
+
+void dfs (int parent, int node) {
+    vis[node] = 1;
+    low[node] = lvl[node];
+    d.push_back(node);
+    
+    bool articulation = 0;
+    
+    int children = 0;
+    for (int i = 0; i < (int) v[node].size(); ++i) {
+        int nxt = v[node][i];
+        if (nxt == parent) {
             continue;
         }
-        niv[vecin] = niv[nod] + 1;
-        dfs(nod, vecin);
-        low[nod] = min(low[nod], low[vecin]);
-        if(low[vecin] >= niv[nod])
-        {
-            nr++;
+        if (vis[nxt]) {
+            low[node] = min(low[node], lvl[nxt]);
+            
+            if (lvl[node] > lvl[nxt]) {
+                edges.insert({node, nxt});
+            }
+            else {
+                edges.insert({nxt, node});
+            }
+            continue;
+        }
+        
+        lvl[nxt] = lvl[node] + 1;
+        children++;
+        dfs(node, nxt);
+        low[node] = min(low[node], low[nxt]);
+        
+        if (lvl[node] < lvl[nxt]) {
+            edges.insert({node, nxt});
+        }
+        else {
+            edges.insert({nxt, node});
+        }
+            
+        if (low[nxt] >= lvl[node]) {
+            cnt++;
+            
+            if (parent != 0) {
+                articulation = 1;
+            }
             int lst;
             do
             {
-                sol[nr].push_back(d.back());
                 lst = d.back();
+                sol[cmt].push_back(d.back());
                 d.pop_back();
-            }while(!d.empty() && lst != vecin);
-            sol[nr].push_back(nod);
+            }while(!d.empty() && lst != nxt);
         }
+        
+        if (low[nxt] > lvl[node]) {
+            // bridge between node and nxt
+            cout << "IMPOSSIBLE\n";
+            exit(0);
+        }
+    }
+    
+    if (parent == 0 && children >= 2) {
+        articulation = 1;
     }
 }
 int main()
 {
-    f >> n >> m;
-    for(int i = 1; i <= m; ++i)
-    {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> m;
+    
+    v.resize(n+1); vis.resize(n+1);
+    lvl.resize(n+1); low.resize(n+1);
+    
+    for (int i = 1; i <= m; ++i) {
         int a, b;
-        f >> a >> b;
+        cin >> a >> b;
         v[a].push_back(b);
         v[b].push_back(a);
     }
+    
     dfs(0, 1);
-    g << nr << '\n';
-    for(int i = 1; i <= nr; g << '\n', ++i)
-        for(int j = 0; j < sol[i].size(); ++j)
-            g << sol[i][j] << " ";
+    
+    if (edges.size() != m) {
+        cout << "IMPOSSIBLE\n";
+        return 0;
+    }
+    
+    for (auto x : edges) {
+        cout << x.first << " " << x.second << '\n';
+    }
     return 0;
 }
