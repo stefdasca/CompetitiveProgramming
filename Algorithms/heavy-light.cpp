098100447ed442ab51869e3980e0de2a1e4a04db
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2024-10-22 16:32:24 +0200

    fix: changed HLD code

--- Algorithms/heavy-light.cpp
+++ Algorithms/heavy-light.cpp
@@ -1,175 +1,224 @@
-// https://www.infoarena.ro/problema/heavypath
-#include<bits/stdc++.h>
+// https://cses.fi/problemset/task/2134/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+ 
 using namespace std;
-ifstream f("heavypath.in");
-ofstream g("heavypath.out");
-// for HLD
-int n, m, v[100002], aint[500002], nrp = 1, path[100002], pos[100002], tt[100002], subtree[100002], lft[100002], pz;
-vector<int>tree[100002], paths[100002];
-/// LCA w/ Euler Tour + segtree
-int p, euler[200002], aint2[800002], niv[100002], lst[100002];
-void dfs1(int dad, int nod)
-{
-    euler[++p] = nod;
-    subtree[nod] = 1;
-    for(int i = 0; i < tree[nod].size(); ++i)
-    {
-        int vecin = tree[nod][i];
-        if(vecin == dad)
+ 
+vector<vector<int>> tree;
+vector<int> values, indices, top, sts, lvl, order, pos;
+ 
+int binjump[18][200005];
+ 
+// LCA + binary lifting
+ 
+void dfs(int parent, int node) {
+    binjump[0][node] = parent;
+    sts[node] = 1;
+    for (int i = 1; i <= 17; i++) {
+        binjump[i][node] = binjump[i-1][binjump[i-1][node]];
+    }
+    
+    for (int i = 0; i < (int) tree[node].size(); i++) {
+        int nxt = tree[node][i];
+        if (nxt == parent) {
             continue;
-        niv[vecin] = niv[nod] + 1;
-        dfs1(nod, vecin);
-        subtree[nod] += subtree[vecin];
-        euler[++p] = nod;
+        }
+        lvl[nxt] = lvl[node] + 1;
+        dfs(node, nxt);
+        sts[node] += sts[nxt];
     }
-    lst[nod] = p;
 }
-void buildlca(int nod, int st, int dr)
-{
-    if(st == dr)
-    {
-        aint2[nod] = st;
-        return;
+ 
+int LCA(int a, int b) {
+    if (lvl[b] < lvl[a]) {
+        swap(a, b);
     }
-    int mid = (st + dr) / 2;
-    buildlca(nod << 1, st, mid);
-    buildlca(nod << 1|1, mid + 1, dr);
-    if(niv[euler[aint2[nod << 1]]] <= niv[euler[aint2[nod << 1|1]]])
-        aint2[nod] = aint2[nod << 1];
-    else
-        aint2[nod] = aint2[nod << 1|1];
-}
-int ana = -1;
-void querylca(int nod, int st, int dr, int L, int R)
-{
-    if(dr < L || st > R)
-        return;
-    if(L <= st && dr <= R)
-    {
-        if(ana == -1)
-            ana = aint2[nod];
-        else
-            if(niv[euler[ana]] > niv[euler[aint2[nod]]])
-                ana = aint2[nod];
-        return;
+    
+    for (int i = 17; i >= 0; i--) {
+        if (lvl[b] - (1<<i) >= lvl[a]) {
+            b = binjump[i][b];
+        }
     }
-    int mid = (st + dr) / 2;
-    querylca(nod << 1, st, mid, L, R);
-    querylca(nod << 1|1, mid + 1, dr, L, R);
+    
+    if (a == b) {
+        return a;
+    }
+    
+    for (int i = 17; i >= 0; i--) {
+        if (binjump[i][a] != binjump[i][b]) {
+            a = binjump[i][a];
+            b = binjump[i][b];
+        }
+    }
+    return binjump[0][a];
 }
-/// HLD
-void dfs(int dad, int nod, int cr)
-{
-    path[nod] = cr;
-    tt[nod] = dad;
-    paths[cr].push_back(nod);
-    pos[nod] = paths[cr].size() - 1;
-    int mx = -1;
-    for(int i = 0; i < tree[nod].size(); ++i)
-    {
-        int vecin = tree[nod][i];
-        if(vecin == dad)
+ 
+int cnt = 0;
+void dfs_hld(int parent, int node, int topp) {
+    cnt++;
+    top[node] = topp;
+    pos[node] = cnt;
+    order[cnt] = node;
+    
+    int mx = 0;
+    for (int i = 0; i < (int) tree[node].size(); i++) {
+        int nxt = tree[node][i];
+        if (nxt == parent) {
             continue;
-        if(mx == -1)
-            mx = vecin;
-        else
-            if(subtree[vecin] > subtree[mx])
-                mx = vecin;
+        }
+        if (mx == 0 || sts[nxt] > sts[mx]) {
+            mx = nxt;
+        }
     }
-    for(int i = 0; i < tree[nod].size(); ++i)
-    {
-        int vecin = tree[nod][i];
-        if(vecin == dad)
+    
+    if (mx != 0) {
+        dfs_hld(node, mx, topp);
+    }
+    
+    for (int i = 0; i < (int) tree[node].size(); i++) {
+        int nxt = tree[node][i];
+        if (nxt == parent) {
             continue;
-        if(mx == vecin)
-            dfs(nod, vecin, cr);
-        else
-            dfs(nod, vecin, ++nrp);
+        }
+        if (nxt != mx) {
+            dfs_hld(node, nxt, nxt);
+        }
     }
 }
-int pc;
-void buildhld(int stp, int nod, int st, int dr)
-{
-    if(st == dr)
-    {
-        aint[nod + stp] = v[paths[pc][st - 1]];
+ 
+vector<int> segtree, start_node, len, start_pos;
+ 
+void build (int start_node, int node, int L, int R) {
+    if (L == R) {
+        segtree[start_node + node - 1] = values[order[L]];
         return;
     }
-    int mid = (st + dr) / 2;
-    buildhld(stp, nod * 2, st, mid);
-    buildhld(stp, nod * 2 + 1, mid + 1, dr);
-    aint[nod + stp] = max(aint[nod * 2 + stp], aint[nod * 2 + 1 + stp]);
+    
+    int mid = (L + R) / 2;
+    build(start_node, node << 1, L, mid);
+    build(start_node, node << 1|1, mid+1, R);
+    
+    segtree[start_node + node - 1] = max(segtree[start_node + (node << 1) - 1], segtree[start_node + (node << 1|1) - 1]);
 }
-int ans;
-void queryhld(int stp, int nod, int st, int dr, int L, int R)
-{
-    if(L <= st && dr <= R)
-    {
-        ans = max(ans, aint[nod + stp]);
+ 
+void update (int start_node, int node, int L, int R, int pos) {
+    if (L == R) {
+        segtree[start_node + node - 1] = values[order[L]];
         return;
     }
-    if(dr < L || st > R)
-        return;
-    int mid = (st + dr) / 2;
-    queryhld(stp, nod * 2, st, mid, L, R);
-    queryhld(stp, nod * 2 + 1, mid + 1, dr, L, R);
-}
-void updatehld(int stp, int nod, int st, int dr, int poz, int val)
-{
-    if(st == dr)
-    {
-        aint[nod + stp] = val;
-        return;
+    
+    int mid = (L + R) / 2;
+    if (pos <= mid) {
+        update (start_node, node << 1, L, mid, pos);
     }
-    int mid = (st + dr) / 2;
-    if(poz <= mid)
-        updatehld(stp, nod * 2, st, mid, poz, val);
-    else
-        updatehld(stp, nod * 2 + 1, mid + 1, dr, poz, val);
-    aint[nod + stp] = max(aint[nod * 2 + stp], aint[nod * 2 + 1 + stp]);
+    else {
+        update (start_node, node << 1|1, mid+1, R, pos);
+    }
+    segtree[start_node + node - 1] = max(segtree[start_node + (node << 1) - 1], segtree[start_node + (node << 1|1) - 1]);
 }
-int main()
-{
-    f >> n >> m;
-    for(int i = 1; i <= n; ++i)
-        f >> v[i];
-    for(int i = 1; i < n; ++i)
-    {
+ 
+int query (int start_node, int node, int L, int R, int Lq, int Rq) {
+    if (R < Lq || L > Rq) {
+        return -1;
+    }
+    
+    if (Lq <= L && R <= Rq) {
+        return segtree[start_node + node - 1];
+    }
+    
+    int mid = (L + R) / 2;
+    return max(query(start_node, node << 1, L, mid, Lq, Rq), query(start_node, node << 1|1, mid+1, R, Lq, Rq));
+}
+ 
+void init(int n) {
+    values.resize(n+1);
+    indices.resize(n+1);
+    top.resize(n+1);
+    tree.resize(n+1);
+    sts.resize(n+1);
+    lvl.resize(n+1);
+    order.resize(n+1);
+    segtree.resize(4*n+1);
+    start_node.resize(n+1);
+    len.resize(n+1);
+    pos.resize(n+1);
+    start_pos.resize(n+1);
+} 
+ 
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, q;
+    cin >> n >> q;
+    
+    init(n);
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> values[i];
+    }
+    
+    for (int i = 1; i < n; i++) {
         int a, b;
-        f >> a >> b;
+        cin >> a >> b;
+        
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
-    dfs1(0, 1);
-    dfs(0, 1, 1);
-    pz = 1;
-    for(int i = 1; i <= nrp; ++i)
-    {
-        lft[i] = pz;
-        pc = i;
-        pz = pz + 4 * paths[i].size() + 1;
-        buildhld(lft[i] - 1, 1, 1, paths[i].size());
-    }
-    buildlca(1, 1, p);
-    for(int i = 1; i <= m; ++i)
-    {
-        int tip, a, b;
-        f >> tip >> a >> b;
-        if(tip == 0)
-        {
-            updatehld(lft[path[a]] - 1, 1, 1, paths[path[a]].size(), pos[a] + 1, b);
-            continue;
+    
+    dfs(0, 1);
+    dfs_hld(0, 1, 1);
+    
+    int fi_node = 1;
+    for (int i = 1; i <= n; ) {
+        int pos = i;
+        while (pos <= n && top[order[pos]] == top[order[i]]) {
+            pos++;
         }
-        ana = -1;
-        querylca(1, 1, p, min(lst[a], lst[b]), max(lst[a], lst[b]));
-        int LCA = euler[ana];
-        ans = -2000000001;
-        while(path[a] != path[LCA])
-            queryhld(lft[path[a]] - 1, 1, 1, paths[path[a]].size(), 1, pos[a] + 1), a = tt[paths[path[a]][0]];
-        while(path[b] != path[LCA])
-            queryhld(lft[path[b]] - 1, 1, 1, paths[path[b]].size(), 1, pos[b] + 1), b = tt[paths[path[b]][0]];
-        queryhld(lft[path[a]] - 1, 1, 1, paths[path[a]].size(), pos[LCA] + 1, max(pos[a], pos[b]) + 1);
-        g << ans << '\n';
+        for (int j = i; j < pos; j++) {
+            start_node[order[j]] = fi_node;
+            len[order[j]] = pos - i;
+            start_pos[order[j]] = i;
+        }
+        build(fi_node, 1, i, pos-1);
+        fi_node += 4 * (pos - i);
+        i = pos;
+    }
+    
+    for (; q; q--) {
+        int type;
+        cin >> type;
+        
+        if (type == 1) {
+            int node, val;
+            cin >> node >> val;
+            values[node] = val;
+            update (start_node[node], 1, start_pos[node], start_pos[node] + len[node] - 1, pos[node]);
+        }
+        else {
+            int L, R;
+            cin >> L >> R;
+            
+            int val = LCA(L, R);
+            int ans = 0;
+            while (order[start_pos[L]] != order[start_pos[val]]) {
+                ans = max(ans, query(start_node[L], 1, start_pos[L], start_pos[L] + len[L] - 1, start_pos[L], pos[L]));
+                int node = order[start_pos[L]];
+                L = binjump[0][node];
+            }
+            
+            while (order[start_pos[R]] != order[start_pos[val]]) {
+                ans = max(ans, query(start_node[R], 1, start_pos[R], start_pos[R] + len[R] - 1, start_pos[R], pos[R]));
+                int node = order[start_pos[R]];
+                R = binjump[0][node];
+            }
+            
+            ans = max(ans, query(start_node[val], 1, start_pos[val], start_pos[val] + len[val] - 1, pos[val], max(pos[L], pos[R])));
+            cout << ans << " ";
+        }
     }
     return 0;
 }
