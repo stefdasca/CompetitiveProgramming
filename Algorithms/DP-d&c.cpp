220b00fe704be3d452edc661eb1c7baa0b572a9d
Stefan Dascalescu <cosmin_stefan_d@yahoo.com>
2025-02-05 23:39:08 +0100

    refactored D&C dp code

--- Algorithms/DP-d&c.cpp
+++ Algorithms/DP-d&c.cpp
@@ -1,50 +1,63 @@
-// https://infoarena.ro/problema/cubeon
+// based on https://cses.fi/problemset/task/2086/ 
 
-#include<bits/stdc++.h>
-#pragma GCC optimize("O3")
-using namespace std;
-int n, k, t;
-long long a[1000002], sp[1000002];
-long long dp[2][1000002];
-long long C(int a, int b)
-{
-    long long val = sp[b] - sp[a-1];
-    return val*val*val;
-}
-void compute(int i, int st, int dr, int optst, int optdr)
-{
-    if(st > dr)
+#include <iostream>
+#include <vector>
+ 
+std::vector<std::vector<long long>> dp, sq;
+
+// the main idea when it comes to d&c dp is to observe that if the quadrangle inequality holds 
+// (or in layman terms, opt[i][j] <= opt[i][j+1] for all possible positions, we can apply this
+// optimization
+
+void divide (int layer, int L, int R, int optL, int optR) {
+    if (L > R) {
         return;
-    int mid = (st + dr) / 2;
-    pair<long long, int> best = {(1LL<<60), -1};
-    for (int k = optst; k <= min(mid, optdr); k++)
-        best = min(best, {dp[1-i][k-1] + C(k, mid), k});
-    dp[i][mid] = best.first;
-    int opt = best.second;
-    compute(i, st, mid - 1, optst, opt);
-    compute(i, mid + 1, dr, opt, optdr);
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> t;
-    for(; t; --t)
-    {
-        cin >> n >> k;
-        for(int i = 1; i <= n; ++i)
-            cin >> a[i];
-        for(int i = 1; i <= n; ++i)
-            sp[i] = sp[i-1] + a[i];
-        long long ss = 0;
-        for(int i = 1; i <= n; ++i)
-        {
-            ss = ss + a[i];
-            dp[1][i] = ss * ss * ss;
-        }
-        for(int i = 2; i <= k; ++i)
-            compute(i%2, i, n, i, n);
-        cout << dp[k%2][n] << '\n';
     }
+    long long bst = (1LL<<60);
+    int whoo = -1;
+    int mid = (L + R) / 2;
+    for (int i = optL; i <= std::min(mid-1, optR); i++) {
+        if (dp[layer-1][i] + sq[i+1][mid] < bst) {
+            bst = dp[layer-1][i] + sq[i+1][mid];
+            whoo = i;
+        }
+    }
+    dp[layer][mid] = bst;
+    divide(layer, L, mid-1, optL, whoo);
+    divide(layer, mid+1, R, whoo, optR);
+}
+ 
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+    
+    std::vector<int> v(n+1);
+    for (int i = 1; i <= n; i++) {
+        std::cin >> v[i];
+    }
+    
+    dp.resize(k+1, std::vector<long long> (n+1));
+    sq.resize(n+1, std::vector<long long> (n+1));
+    
+    for (int i = 1; i <= n; i++) {
+        int sum = 0;
+        for (int j = i; j <= n; j++) {
+            sum += v[j];
+            sq[i][j] = 1LL * sum * sum;
+        }
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        dp[1][i] = sq[1][i];
+    }
+    
+    for (int i = 2; i <= k; i++) {
+        for (int pos = 0; pos + 1 < i; pos++) {
+            dp[i-1][pos] = (1LL<<60);
+        }
+        divide(i, i, n, i-1, n);
+    }
+    
+    std::cout << dp[k][n] << '\n';
     return 0;
 }
